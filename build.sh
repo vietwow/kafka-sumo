302ebd209d1a1556a56d1bf3656e1e1ff088f9e2
Viet Nguyen <vietwow@gmail.com>
2019-02-25 16:05:11 +0700

    update

--- build.sh
+++ build.sh
@@ -7,4 +7,5 @@ export BUILD_NUMBER="1.0"
 export SHA1=`git rev-parse HEAD`
 export DATE=$(date +%Y_%m_%d)
 
-CGO_ENABLED=0 GO111MODULE=on GOOS=linux go build '-mod=vendor'  -ldflags  " -X main.version=$BUILD_NUMBER -X main.commit_sha1=$SHA1 -X main.builddate=$DATE " -a -installsuffix cgo -o ../binary-builded/kafka-sumo ./cmd/forwarder/
+#CGO_ENABLED=0 GO111MODULE=on GOOS=linux go build '-mod=vendor'  -ldflags  " -X main.version=$BUILD_NUMBER -X main.commit_sha1=$SHA1 -X main.builddate=$DATE " -a -installsuffix cgo -o ../binary-builded/kafka-sumo ./cmd/forwarder/
+go build cmd/forwarder/main.go
