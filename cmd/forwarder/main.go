4359d4dc722b4467542ca88a00f91ac299880a8f
Viet Nguyen <vietwow@gmail.com>
2019-03-04 18:08:29 +0700

    refactor 2

--- cmd/forwarder/main.go
+++ cmd/forwarder/main.go
@@ -3,7 +3,7 @@ package main
 import (
     "fmt"
     "os"
-    // "github.com/confluentinc/confluent-kafka-go/kafka"
+    "github.com/confluentinc/confluent-kafka-go/kafka"
     "io/ioutil"
     "time"
     "strings"
